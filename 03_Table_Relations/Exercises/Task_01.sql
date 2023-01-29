8cbed2dee064b71930acf124ff89512855e408a3
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2023-01-29 22:44:42 +0200

    100 / 100

---
+++ 03_Table_Relations/Exercises/Task_01.sql
@@ -0,0 +1,35 @@
+-- 01. One-To-One Relationship
+CREATE DATABASE Test
+
+USE Test
+
+CREATE TABLE Persons
+(
+PersonID INT NOT NULL,
+FirstName VARCHAR(50) NOT NULL,
+Salary DECIMAL(7, 2) NOT NULL,
+PassportID INT UNIQUE NOT NULL
+)
+
+CREATE TABLE Passports
+(
+PassportID INT PRIMARY KEY NOT NULL,
+PassportNumber CHAR(8) NOT NULL
+)
+
+INSERT INTO Persons(PersonID, FirstName, Salary, PassportID)
+VALUES (1, 'Roberto', 43300.00, 102),
+       (2, 'Tom', 56100.00, 103),
+	   (3, 'Yana', 60200.00, 101) 
+
+INSERT INTO Passports(PassportID, PassportNumber)
+VALUES (101, 'N34FG21B'),
+       (102, 'K65LO4R7'),
+	   (103, 'ZE657QP2')
+
+ALTER TABLE Persons
+ADD CONSTRAINT PK_Persons_PersonID PRIMARY KEY (PersonID)
+
+ALTER TABLE Persons
+ADD CONSTRAINT FK_Persons_Passports 
+FOREIGN KEY (PassportID) REFERENCES Passports(PassportID)
